f33f3efdfb7885147435449ff120702ceb991890
Dario de souza <darioo_dario@hotmail.com>
2024-06-19 20:32:54 -0300

    chore: fetch products

---
+++ src/services/api/fetch.data.tsx
@@ -0,0 +1,7 @@
+export let baseUrl = "https://fakestoreapi.com/products";
+export async function fetchDataProducts<T>(url: string) {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error("Não foi possivel encontrar Dados");
+  const data = (await response.json()) as T;
+  return data;
+}
