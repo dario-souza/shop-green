e706ac3068f17ac1c83eab8b36b31a8b3604f7da
Dario de souza <darioo_dario@hotmail.com>
2024-06-25 20:40:12 -0300

    chore: create component product details

---
+++ src/app/(pages)/details/[id]/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { CardDetails } from '@/components/card.details'
+import { Container } from '@/components/container'
+import { baseUrl, fetchDataProducts } from '@/services/api/fetch.data'
+import { Product } from '@/types/products.type'
+
+type ProductDetailsProps = {
+  params: {
+    id: string
+  }
+}
+
+const ProductDetails = async ({ params: { id } }: ProductDetailsProps) => {
+  const data = await fetchDataProducts<Product>(`${baseUrl}/${id}`)
+  return (
+    <Container className='h-[calc(100vh-64px)] flex justify-center items-center'>
+      <CardDetails data={data} />
+    </Container>
+  )
+}
+
+export default ProductDetails
