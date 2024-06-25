b4222909fd256596412d3fe64425bc5f8e959195
Dario de souza <darioo_dario@hotmail.com>
2024-06-25 20:41:24 -0300

    chore: component product card

--- src/components/product.card.tsx
+++ src/components/product.card.tsx
@@ -10,6 +10,7 @@ import { StrongCard } from './strongcard'
 import { convertToBRL } from '@/helper/convert.to.brl'
 import { RemoveCart } from './icons/removecart'
 import { AddCart } from './icons/addcart'
+import Link from 'next/link'
 
 
 interface ProductCardProps {
@@ -20,6 +21,7 @@ interface ProductCardProps {
 
 const ProductCard = ({ data, toggleCart, cart }: ProductCardProps) => {
   const isInCart = cart.some(item => item.id === data.id);
+  console.log(cart)
   return (
     <div className='text-center border rounded-md p-2 sm:p-4'>
       <Image className='mx-auto h-28 md:h-[200px] object-contain' src={data.image} alt={data.title} width={1049} height={1500} sizes='(max-width: 600px) 100vw' />
@@ -28,7 +30,7 @@ const ProductCard = ({ data, toggleCart, cart }: ProductCardProps) => {
         <StrongCard className='font-extrabold'>{convertToBRL(data.price)}</StrongCard>
       </PriceCard>
       <div className='flex justify-between mt-4'>
-        <span>Ver mais</span>
+        <Link href={`/details/${data.id}`}>Ver mais</Link>
         <Button className='bg-transparent' onClick={() => toggleCart(data)}>{isInCart ? <RemoveCart /> : <AddCart />}</Button>
       </div>
     </div>
