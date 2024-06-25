d978e8e413db7f8be4ed246bd152eb9bf7073f4c
Dario de souza <darioo_dario@hotmail.com>
2024-06-25 20:41:37 -0300

    chore: component title card

--- src/components/titlecard.tsx
+++ src/components/titlecard.tsx
@@ -7,7 +7,7 @@ export const TitleCard = ({ children, className, ...rest }: TitleCardProps) => {
   return (
     <h2
       {...rest}
-      className={twMerge('line-clamp-1 text-lg sm:text-xl md:text-2xl font-semibold', className)}>
+      className={twMerge('text-center line-clamp-1 text-2xl sm:text-3xl md:text-4xl font-bold', className)}>
       {children}
     </h2>
   )
