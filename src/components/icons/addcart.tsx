97f7d808a79b1625732a7c61e3529188cd7bf60b
Dario de souza <darioo_dario@hotmail.com>
2024-06-25 11:23:51 -0300

    chore: component add e remove cart

---
+++ src/components/icons/addcart.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { FaCartPlus } from "react-icons/fa";
+
+export const AddCart = () => {
+  return (
+    <FaCartPlus className='text-green-500' size={25} />
+  )
+}
---
+++ src/components/icons/removecart.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { BsCartDashFill } from "react-icons/bs";
+
+export const RemoveCart = () => {
+  return (
+    <BsCartDashFill className='text-red-500' size={25} />
+  )
+}
