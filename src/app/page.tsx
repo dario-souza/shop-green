993488e523d92f791a044fcb13b6e1b64278d946
Dario de souza <darioo_dario@hotmail.com>
2024-07-31 19:09:33 -0300

    style: adicionado footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@/components/container";
+import { Footer } from "@/components/footer/footer";
 import ProductsCards from "@/components/products/products.cards";
 import { baseUrl, fetchDataProducts } from "@/services/api/fetch.data";
 import { Product } from "@/types/products.type";
@@ -6,8 +7,11 @@ import { Product } from "@/types/products.type";
 export default async function Home() {
   const data = await fetchDataProducts<Product[]>(baseUrl)
   return (
-    <Container>
-      <ProductsCards product={data} />
-    </Container>
+    <main>
+      <Container>
+        <ProductsCards product={data} />
+      </Container>
+      <Footer />
+    </main>
   );
 }
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className='h-20 bg-emerald-400 mt-4 p-4'>
+      <p className='text-center text-zinc-950 text-xs'>&copy;Todos os direitos reservados by <a href="https://www.dariosouza.tech/" className='hover:underline font-semibold' target='_blank'>Dario de souza</a> - 2024</p>
+    </footer>
+  )
+}
